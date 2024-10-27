cdb5040b9bd74cdb3a0da95fcabcf68a5dfeb902
Ivan Villarroel <ivillarroel@SCLELECVAV01.turner.com>
2024-10-27 16:18:49 -0300

    ZONES

--- configuracion.php
+++ configuracion.php
@@ -65,7 +65,7 @@
 //	INFORMACION DE LA APLICACION
 
 //	$_CONF_VERSION						=	"?alpha.".time();
-	$_CONF_VERSION						=	"?version.5.9";
+	$_CONF_VERSION						=	"?version.6";
 
 //	-		-		-		-		-		-		-		-		-		-		-		-		-		-		-		-	
 //	CREACION DE URLS AMIGABLES
