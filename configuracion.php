b0ac4270d1cd477c16fc9e3ca1fcc063a37adecb
Ivan Villarroel <ivillarroel@SCLELECVAV01.turner.com>
2025-11-16 15:04:29 -0300

    Bug fixed - Nombres zonas

--- configuracion.php
+++ configuracion.php
@@ -65,7 +65,7 @@
 //	INFORMACION DE LA APLICACION
 
 //	$_CONF_VERSION						=	"?alpha.".time();
-	$_CONF_VERSION						=	"?version.13.4";
+	$_CONF_VERSION						=	"?version.13.5";
 
 //	-		-		-		-		-		-		-		-		-		-		-		-		-		-		-		-	
 //	CREACION DE URLS AMIGABLES
